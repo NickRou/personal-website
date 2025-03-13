c82c6800aaa9e3262f0c377e15fc9dcdb96fbb12
Nicklas Rouvinen <nicklas.rouvinen@gmail.com>
2025-03-12 23:07:03 -0700

    add default image service to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
@@ -8,5 +8,8 @@ export default defineConfig({
   integrations: [tailwind()],
   experimental: {
     svg: true,
+  },
+  image: {
+    service: passthroughImageService(),
   },
 });
